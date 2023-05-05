f6206b8959bc000285578353d42985a6c2666550
MichalBerg99 <36158328+MichalBerg99@users.noreply.github.com>
2023-05-05 10:09:40 +0200

    Add files via upload

---
+++ ApplicationDeployment.ps1
@@ -0,0 +1,322 @@
+﻿if (-not (Get-Command ConvertTo-Yaml -ErrorAction SilentlyContinue)) {
+    [System.Windows.MessageBox]::Show("The powershell-yaml module is required. Please install it using 'Install-Module -Name powershell-yaml'.")
+    return
+}
+
+# Load WPF and required assemblies
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName PresentationCore
+
+# Define WPF XAML
+[xml]$xaml = @"
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        Title="Application Deployment" Height="500" Width="500">
+    <Grid Margin="10">
+        <Grid.RowDefinitions>
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="*" />
+        </Grid.RowDefinitions>
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="Auto" />
+            <ColumnDefinition Width="*" />
+        </Grid.ColumnDefinitions>
+        <Label Grid.Row="0" Grid.Column="0" Content="Resource Group:" />
+        <ComboBox Grid.Row="0" Grid.Column="1" Name="ResourceGroupComboBox" Margin="5" />
+        <Label Grid.Row="1" Grid.Column="0" Content="Git Repository:" />
+        <TextBox Grid.Row="1" Grid.Column="1" Name="GitRepoTextBox" Margin="5" />
+        <Label Grid.Row="2" Grid.Column="0" Content="Azure Container Registry (ACR) Name:" />
+        <ComboBox Grid.Row="2" Grid.Column="1" Name="ACRNameComboBox" Margin="5" />
+        <Label Grid.Row="3" Grid.Column="0" Content="Azure Kubernetes Service (AKS) Name:" />
+        <ComboBox Grid.Row="3" Grid.Column="1" Name="AKSNameComboBox" Margin="5" />
+        <Label Grid.Row="4" Grid.Column="0" Content="Namespace (Optional):" />
+        <TextBox Grid.Row="4" Grid.Column="1" Name="NamespaceTextBox" Margin="5" />
+        <Label Grid.Row="5" Grid.Column="0" Content="Networking Capabilities:" />
+        <ListBox Grid.Row="5" Grid.Column="1" Name="NetworkingCapabilitiesListBox" Margin="5" SelectionMode="Multiple">
+            <ListBoxItem>Storage Account</ListBoxItem>
+            <ListBoxItem>Redis</ListBoxItem>
+            <ListBoxItem>Database</ListBoxItem>
+        </ListBox>
+        <StackPanel Grid.Row="6" Grid.Column="1" Name="StorageAccountPanel" Margin="5" Visibility="Collapsed">
+            <Label Content="Storage Account:" />
+            <ComboBox Name="StorageAccountComboBox" />
+        </StackPanel>
+        <StackPanel Grid.Row="7" Grid.Column="1" Name="DatabasePanel" Margin="5" Visibility="Collapsed">
+            <Label Content="Database:" />
+            <ComboBox Name="DatabaseComboBox" />
+        </StackPanel>
+        <Button Grid.Row="8" Grid.Column="1" Name="DeployButton" Content="Deploy" Margin="5" />
+    </Grid>
+</Window>
+"@
+
+# Create WPF window
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+$window = [Windows.Markup.XamlReader]::Load($reader)
+
+# Find WPF elements
+$resourceGroupComboBox = $window.FindName('ResourceGroupComboBox')
+$gitRepoTextBox = $window.FindName('GitRepoTextBox')
+$acrNameComboBox = $window.FindName('ACRNameComboBox')
+$aksNameComboBox = $window.FindName('AKSNameComboBox')
+$namespaceTextBox = $window.FindName('NamespaceTextBox')
+$networkingCapabilitiesListBox = $window.FindName('NetworkingCapabilitiesListBox')
+$storageAccountPanel = $window.FindName('StorageAccountPanel')
+$storageAccountComboBox = $window.FindName('StorageAccountComboBox')
+$databasePanel = $window.FindName('DatabasePanel')
+$databaseComboBox = $window.FindName('DatabaseComboBox')
+$deployButton = $window.FindName('DeployButton')
+
+# Get existing resource groups, ACRs, and AKS clusters
+Connect-AzAccount
+$allResourceGroups = Get-AzResourceGroup | Select-Object -ExpandProperty ResourceGroupName
+$resourceGroupComboBox.ItemsSource = $allResourceGroups
+
+# Resource group selection changed event handler
+$resourceGroupComboBox.Add_SelectionChanged({
+$selectedResourceGroup = $resourceGroupComboBox.SelectedItem
+
+if ($selectedResourceGroup) {
+    # Get ACRs and AKS clusters in the selected resource group
+    $allAcrs = Get-AzContainerRegistry -ResourceGroupName $selectedResourceGroup
+    $allAks = Get-AzAksCluster -ResourceGroupName $selectedResourceGroup
+
+    # Populate the ACR and AKS ComboBoxes
+    $acrNameComboBox.Items.Clear()
+    $aksNameComboBox.Items.Clear()
+    foreach ($acr in $allAcrs) {
+        $acrNameComboBox.Items.Add($acr.Name) | Out-Null
+    }
+    foreach ($aks in $allAks) {
+        $aksNameComboBox.Items.Add($aks.Name) | Out-Null
+    }
+
+    # Get storage accounts and databases in the selected resource group
+    $allStorageAccounts = Get-AzStorageAccount -ResourceGroupName $selectedResourceGroup
+    $allDatabases = Get-AzSqlServer -ResourceGroupName $selectedResourceGroup
+
+    # Populate the storage account and database ComboBoxes
+    $storageAccountComboBox.Items.Clear()
+    $databaseComboBox.Items.Clear()
+    foreach ($storageAccount in $allStorageAccounts) {
+        $storageAccountComboBox.Items.Add($storageAccount.Name) | Out-Null
+    }
+    foreach ($database in $allDatabases) {
+        $databaseComboBox.Items.Add($database.Name) | Out-Null
+    }
+
+    # Show or hide the storage account and database panels
+    if ($storageAccountComboBox.Items.Count -gt 0) {
+        $storageAccountPanel.Visibility = 'Visible'
+    } else {
+        $storageAccountPanel.Visibility = 'Collapsed'
+    }
+
+    if ($databaseComboBox.Items.Count -gt 0) {
+        $databasePanel.Visibility = 'Visible'
+    } else {
+        $databasePanel.Visibility = 'Collapsed'
+    }
+}
+
+})
+
+
+# Deploy button click event handler
+$deployButton.Add_Click({
+$gitRepo = $gitRepoTextBox.Text
+$acrName = $acrNameComboBox.SelectedItem.ToLower()
+$aksName = $aksNameComboBox.SelectedItem
+$networkingCapabilities = $networkingCapabilitiesListBox.SelectedItems
+$selectedResourceGroup = $resourceGroupComboBox.SelectedItem
+$namespace = $namespaceTextBox.Text
+if (-not $namespace) {
+    $namespace = "default"
+}
+
+Write-Host "Namespace: $namespace"
+
+
+# Deploy the container image to AKS
+$aks = Get-AzAksCluster -Name $aksName -ResourceGroupName $selectedResourceGroup
+az aks get-credentials --name $aksName --resource-group $selectedResourceGroup
+
+# Create the namespace if it doesn't exist
+$namespaceExists = kubectl get namespaces --no-headers -o custom-columns=":metadata.name" | Where-Object { $_ -eq $namespace }
+
+if (-not $namespaceExists) {
+    kubectl create namespace $namespace
+}
+
+
+if (-not ($selectedResourceGroup -and $gitRepo -and $acrName -and $aksName)) {
+    [System.Windows.MessageBox]::Show("All fields must be filled in.")
+    return
+}
+
+# Test the Git repository URL
+    $gitRepoTest = git ls-remote -q $gitRepo 2>&1
+    if ($gitRepoTest -like "*fatal:*") {
+        [System.Windows.MessageBox]::Show("Invalid Git repository URL. Please provide a valid URL.")
+        return
+    }
+
+# Clone the Git repository
+$tempFolder = New-Item -ItemType Directory -Path (Join-Path $env:TEMP (Get-Random))
+git clone $gitRepo $tempFolder 2>&1
+
+# Login to Azure
+Connect-AzAccount
+
+# Build and push the container image to ACR
+$acr = Get-AzContainerRegistry -Name $acrName -ResourceGroupName $selectedResourceGroup
+az acr login --name $acrName
+$imageName = "$($acr.LoginServer)/app:latest"
+docker build -t $imageName $tempFolder
+Write-host "Building Docker image"
+Start-Sleep -Seconds 5
+Write-host "Pushing the Docker image"
+docker push $imageName
+Write-host "Docker image has been pushed to the container registry"
+
+
+# Apply networking capabilities
+$envVars = @()
+if ($networkingCapabilities -contains 'Storage Account') {
+    $selectedStorageAccount = $storageAccountComboBox.SelectedItem
+    if ($selectedStorageAccount) {
+        $storageAccountConnectionString = (Get-AzStorageAccount -Name $selectedStorageAccount -ResourceGroupName $selectedResourceGroup).Context.ConnectionString
+        $envVars += @{
+            name  = "STORAGE_ACCOUNT_CONNECTION_STRING"
+            value = $storageAccountConnectionString
+        }
+    }
+}
+
+$redisContainerYaml = ""
+#$redisPassword = [Convert]::ToBase64String((New-Guid).ToByteArray()).TrimEnd('=')
+#$secretName = "redis-password-$aksName" # or any other unique identifier
+#kubectl create secret generic $secretName --from-literal=password=$redisPassword
+
+# Stores the Redis container YAML definition if Redis is selected
+if ($networkingCapabilities -like "*Redis") {
+    $envVars += @{
+        name  = "REDIS_HOST"
+        value = "redis"
+    }
+
+    $redisContainerYaml = @"
+
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: redis
+  namespace: $namespace
+spec:
+  selector:
+    app: redis
+  ports:
+    - protocol: TCP
+      port: 6379
+      targetPort: 6379
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: redis
+  namespace: $namespace
+spec:
+  selector:
+    matchLabels:
+      app: redis
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: redis
+    spec:
+      containers:
+        - name: redis
+          image: redis:6.2
+          ports:
+            - containerPort: 6379
+"@
+}
+
+
+Write-Host "Preparing environment variables for YAML deployment"
+
+if ($networkingCapabilities -contains 'External Database') {
+    $selectedDatabase = $databaseComboBox.SelectedItem
+    if ($selectedDatabase) {
+        $databaseConnectionString = (Get-AzSqlServer -Name $selectedDatabase -ResourceGroupName $selectedResourceGroup).Context.ConnectionString
+        $envVars += @{
+            name  = "DATABASE_CONNECTION_STRING"
+            value = $databaseConnectionString
+        }
+    }
+}
+
+$envYaml = ""
+foreach ($envVar in $envVars) {
+    $envYaml += @"
+        - name: $($envVar.name)
+          value: $($envVar.value)
+"@
+}
+
+$deploymentYaml = @"
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: app-deployment
+  namespace: $namespace
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: app
+  template:
+    metadata:
+      labels:
+        app: app
+    spec:
+      imagePullSecrets:
+      - name: my-secret
+      containers:
+      - name: app-container
+        image: $imageName
+        env:
+$envYaml
+"@
+
+
+$deploymentYaml += $redisContainerYaml
+
+Write-Host "Creating deployment YAML with environment variables"
+
+Set-Content -Path "$tempFolder\deployment.yaml" -Value $deploymentYaml
+Write-Host "Saving deployment YAML file to disk"
+kubectl apply -f "$tempFolder\deployment.yaml" --namespace=$namespace
+Write-Host "Applying deployment YAML to Azure Kubernetes Cluster"
+# Clean up
+# Remove-Item -Recurse -Force $tempFolder
+
+# Show success message
+[System.Windows.MessageBox]::Show("Deployment completed!")
+
+})
+
+# Show WPF window
+$window.ShowDialog() | Out-Null
---
+++ AzureResourceSetup.ps1
@@ -0,0 +1,186 @@
+﻿# Log in to your Azure account with a specific tenant
+$tenantId = "ace1cb30-a5d7-4184-95b2-800ff3963db0"
+Connect-AzAccount -TenantId $tenantId
+
+# Load the required assemblies for WPF
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName System.Drawing
+
+# XAML for WPF form
+[xml]$xaml = @"
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="Enter Variables" Height="420" Width="400">
+    <Grid>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="Auto" />
+            <RowDefinition Height="*" />
+            <RowDefinition Height="Auto" />
+        </Grid.RowDefinitions>
+        <ScrollViewer Grid.Row="1" VerticalScrollBarVisibility="Auto">
+            <StackPanel Margin="10">
+                <TextBlock Text="Resource Group:" />
+                <TextBox x:Name="ResourceGroup" />
+
+                <TextBlock Text="Location:" Margin="0,10,0,0" />
+                <ComboBox x:Name="Location" />
+                
+                <TextBlock Text="Container Registry Name:" Margin="0,10,0,0" />
+                <TextBox x:Name="ContainerRegistryName" />
+
+                <TextBlock Text="AKS Cluster Name:" Margin="0,10,0,0" />
+                <TextBox x:Name="AKSClusterName" />
+
+                <TextBlock Text="Node Count:" Margin="0,10,0,0" />
+                <ComboBox x:Name="NodeCount" />
+
+                <TextBlock Text="Existing Resource Group:" Margin="0,10,0,0" />
+                <StackPanel Orientation="Horizontal">
+                    <ComboBox x:Name="ExistingResourceGroup" Width="250" IsEnabled="False" />
+                    <CheckBox x:Name="UseExistingResourceGroup" Content="Use Existing" Margin="10,0,0,0" />
+                </StackPanel>
+                
+                <TextBlock Text="Existing Container Registry:" Margin="0,10,0,0" />
+                <StackPanel Orientation="Horizontal">
+                    <ComboBox x:Name="ExistingContainerRegistry" Width="250" IsEnabled="False" />
+                    <CheckBox x:Name="UseExistingContainerRegistry" Content="Use Existing" Margin="10,0,0,0" />
+                </StackPanel>
+            </StackPanel>
+        </ScrollViewer>
+        <Button x:Name="OkButton" Content="OK" Grid.Row="2" Width="75" Height="23" Margin="10" HorizontalAlignment="Right" />
+        <ProgressBar x:Name="ProgressBar" Grid.Row="2" Margin="10,0,95,0" Height="23" IsIndeterminate="True" Visibility="Hidden" />
+    </Grid>
+</Window>
+"@
+
+# Create WPF form and add controls
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+$form = [Windows.Markup.XamlReader]::Load($reader)
+
+# Assign variables to WPF controls
+$ResourceGroup = $form.FindName("ResourceGroup")
+$Location = $form.FindName("Location")
+$ContainerRegistryName = $form.FindName("ContainerRegistryName")
+$AKSClusterName = $form.FindName("AKSClusterName")
+$NodeCount = $form.FindName("NodeCount")
+$ExistingResourceGroup = $form.FindName("ExistingResourceGroup")
+$UseExistingResourceGroup = $form.FindName("UseExistingResourceGroup")
+$ExistingContainerRegistry = $form.FindName("ExistingContainerRegistry")
+$UseExistingContainerRegistry = $form.FindName("UseExistingContainerRegistry")
+$OkButton = $form.FindName("OkButton")
+$ProgressBar = $form.FindName("ProgressBar")
+
+# Fill the comboboxes
+$Location.ItemsSource = @("westus")
+$NodeCount.ItemsSource = @(1..5)
+
+$existingResourceGroups = Get-AzResourceGroup
+$ExistingResourceGroup.ItemsSource = $existingResourceGroups.ResourceGroupName
+$existingContainerRegistries = Get-AzContainerRegistry
+foreach ($acr in $existingContainerRegistries) {
+    $ExistingContainerRegistry.Items.Add($acr.Name)
+}
+
+
+# Function to enable/disable TextBox controls based on the CheckBox state
+$UseExistingResourceGroup.Add_Checked({
+    $ResourceGroup.IsEnabled = -not $UseExistingResourceGroup.IsChecked
+    $ExistingResourceGroup.IsEnabled = $UseExistingResourceGroup.IsChecked
+    $Location.IsEnabled = -not $UseExistingResourceGroup.IsChecked
+})
+$UseExistingResourceGroup.Add_Unchecked({
+    $ResourceGroup.IsEnabled = -not $UseExistingResourceGroup.IsChecked
+    $ExistingResourceGroup.IsEnabled = $UseExistingResourceGroup.IsChecked
+    $Location.IsEnabled = -not $UseExistingResourceGroup.IsChecked
+})
+$UseExistingContainerRegistry.Add_Checked({
+    $ContainerRegistryName.IsEnabled = -not $UseExistingContainerRegistry.IsChecked
+    $ExistingContainerRegistry.IsEnabled = $UseExistingContainerRegistry.IsChecked
+})
+$UseExistingContainerRegistry.Add_Unchecked({
+    $ContainerRegistryName.IsEnabled = -not $UseExistingContainerRegistry.IsChecked
+    $ExistingContainerRegistry.IsEnabled = $UseExistingContainerRegistry.IsChecked
+})
+
+$OkButton.Add_Click({
+    $form.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.Close()
+})
+
+# Show the form and get the user input
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+    $useExistingResourceGroup = $UseExistingResourceGroup.IsChecked
+    $useExistingContainerRegistry = $UseExistingContainerRegistry.IsChecked
+
+    if ($useExistingResourceGroup) {
+        $resource_group = $ExistingResourceGroup.Text
+    } else {
+        $resource_group = $ResourceGroup.Text
+    }
+
+    if ($useExistingContainerRegistry) {
+        $container_registry_name = $ExistingContainerRegistry.Text
+    } else {
+        $container_registry_name = $ContainerRegistryName.Text
+    }
+
+    $location = $Location.Text
+    $aks_cluster_name = $AKSClusterName.Text
+    $node_count = $NodeCount.Text
+}
+
+# Convert the $node_count to an integer
+$node_count = [int]$node_count
+
+# Check if the Resource Group exists, if not, create one
+if (-not $useExistingResourceGroup) {
+    New-AzResourceGroup -Name $resource_group -Location $location
+}
+
+Start-Sleep -Seconds 5
+
+# Check if the Container Registry exists, if not, create one
+if (-not $useExistingContainerRegistry) {
+    New-AzContainerRegistry -ResourceGroupName $resource_group -Name $container_registry_name -Sku Basic -EnableAdminUser
+}
+
+Start-Sleep -Seconds 5
+
+# Login to the Container Registry
+$credentials = Get-AzContainerRegistryCredential -ResourceGroupName $resource_group -Name $container_registry_name
+
+Start-Sleep -Seconds 5
+
+# Get the login server address of the container registry
+$acr_login_server = (Get-AzContainerRegistry -ResourceGroupName $resource_group -Name $container_registry_name).LoginServer
+
+Start-Sleep -Seconds 5
+
+# Create an AKS cluster with one node pool and one node
+New-AzAksCluster -ResourceGroupName $resource_group -Name $aks_cluster_name -NodeCount $node_count -EnableManagedIdentity
+
+Start-Sleep -Seconds 5
+
+# Grant the required permissions for 'acrpull' role assignment
+$aksIdentityPrincipalId = (Get-AzAksCluster -ResourceGroupName $resource_group -Name $aks_cluster_name).Identity.PrincipalId
+$acrResourceId = (Get-AzContainerRegistry -ResourceGroupName $resource_group -Name $container_registry_name).Id
+New-AzRoleAssignment -ObjectId $aksIdentityPrincipalId -RoleDefinitionName "AcrPull" -Scope $acrResourceId
+
+Start-Sleep -Seconds 5
+
+Set-AzAksCluster -Name $aks_cluster_name -ResourceGroupName $resource_group -AcrNameToAttach $container_registry_name
+
+Start-Sleep -Seconds 5
+
+# Connect kubectl to the AKS cluster
+Import-AzAksCredential -ResourceGroupName $resource_group -Name $aks_cluster_name
+
+Start-Sleep -Seconds 5
+
+# Connect kubectl to the AKS cluster
+Import-AzAksCredential -ResourceGroupName $resource_group -Name $aks_cluster_name
+
+# Display an alert to inform the user that the script has finished running
+[System.Windows.Forms.MessageBox]::Show('The script has finished running.', 'Script Complete', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
